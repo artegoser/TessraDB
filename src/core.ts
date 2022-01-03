ec92f78c8cbff7b5cb04759f87da30279bcb8d24
Artemy <artegoser@gmail.com>
2022-01-03 14:25:00 +0300

    style: format code to tab-size 2

--- src/core.ts
+++ src/core.ts
@@ -1,56 +1,62 @@
-import * as fs from "fs"
-import * as JSONS from "JSONStream"
-import * as path from "path"
+import * as fs from "fs";
+import * as JSONS from "JSONStream";
+import * as path from "path";
 
 /**
  * Main class of TessraDB
  */
 export class TessraDB {
-    name:string
-    constructor(name:string){
-        this.name = name;
-        this.init();
+  name: string;
+  constructor(name: string) {
+    this.name = name;
+    this.init();
+  }
+  /**
+   * initialize TessraDB
+   */
+  private init() {
+    if (!fs.existsSync(this.name)) {
+      fs.mkdirSync(this.name);
     }
-    /**
-     * initialize TessraDB
-     */
-    private init(){
-        if (!fs.existsSync(this.name)){
-            fs.mkdirSync(this.name);
-        }
+  }
+  /**
+   * A collections object is a read-only variable that contains all the collections in the database
+   * Do not use if the database is large
+   */
+  public get collections() {
+    let collect = {};
+    let filenames = fs.readdirSync(this.name);
+    for (let filename of filenames) {
+      collect[filename] = JSON.parse(
+        fs.readFileSync(path.join(this.name, filename), "utf-8")
+      );
     }
-    /**
-     * A collections object is a read-only variable that contains all the collections in the database
-     * Do not use if the database is large
-     */
-    public get collections(){
-        let collect = {};
-        let filenames = fs.readdirSync(this.name);
-        for(let filename of filenames) {
-            collect[filename] = JSON.parse(fs.readFileSync(path.join(this.name, filename), "utf-8"));
-        }
-        return collect
+    return collect;
+  }
+  public async getCollection(name: string) {
+    try {
+      let file = await fs.promises.readFile(
+        path.join(this.name, name),
+        "utf-8"
+      );
+      return JSON.parse(file);
+    } catch (e) {
+      if (e.errno === -4058) throw new Error("Collection does not exist");
+      else throw e;
     }
-    public async getCollection(name:string){
-        try{
-            let file = await fs.promises.readFile(path.join(this.name, name), "utf-8")
-            return JSON.parse(file);
-        } catch(e){
-            if(e.errno===-4058) throw new Error("Collection does not exist")
-            else throw e;
-        }
-    }
-    /**
-     * Returns names of all collections
-     */
-    public get colNames(){
-        return fs.readdirSync(this.name);
-    }
-    /**
-     * Creates collection
-     */
-    public async createCollection(name:string){
-        if(this.colNames.indexOf(name)<0) await fs.promises.writeFile(path.join(this.name, name), "[]");
-        else throw new Error("Collection already exists. Please, drop collection");
-    }
+  }
+  /**
+   * Returns names of all collections
+   */
+  public get colNames() {
+    return fs.readdirSync(this.name);
+  }
+  /**
+   * Creates collection
+   */
+  public async createCollection(name: string) {
+    if (this.colNames.indexOf(name) < 0)
+      await fs.promises.writeFile(path.join(this.name, name), "[]");
+    else throw new Error("Collection already exists. Please, drop collection");
+  }
 }
--- tests/tests.spec.js
+++ tests/tests.spec.js
@@ -1,78 +1,78 @@
-let {TessraDB} = require("../lib/index");
-let folderstodelete = []
+let { TessraDB } = require("../lib/index");
+let folderstodelete = [];
 
-function genid(){
-    let length = 8;
-    var result = '';
-    var characters = 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789';
-    var charactersLength = characters.length;
-    for ( var i = 0; i < length; i++ ) {
-        result += characters.charAt(Math.floor(Math.random() * 
-        charactersLength));
-    }    
-    return result;
+function genid() {
+  let length = 8;
+  var result = "";
+  var characters =
+    "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789";
+  var charactersLength = characters.length;
+  for (var i = 0; i < length; i++) {
+    result += characters.charAt(Math.floor(Math.random() * charactersLength));
+  }
+  return result;
 }
 
-function createDb(){
-    let db = new TessraDB(genid());
-    folderstodelete.push(db.name);
-    return db;
+function createDb() {
+  let db = new TessraDB(genid());
+  folderstodelete.push(db.name);
+  return db;
 }
 
 let fs = require("fs");
 
-
-function removefiles(dbname){
-    fs.rmdirSync(dbname, { recursive: true });
+function removefiles(dbname) {
+  fs.rmdirSync(dbname, { recursive: true });
 }
 
-describe("FS", ()=>{
-    describe("collections", ()=>{
-
-        after(()=>{
-            for(let folder of folderstodelete){
-                removefiles(folder);
-            }
-        });
-
-        it("create collections", async ()=>{
-            let db = createDb();
-            await db.createCollection("helloworld");
-            if(db.colNames.indexOf("helloworld")<0) throw new Error("collection is not created");
-        });
-
-        it("get collection by name", async ()=>{
-            let db = createDb();
-            await db.createCollection("helloworld");
-            if(!(await db.getCollection("helloworld"))) throw new Error("collection is not created");
-        });
-
-        it("throw error if collection doesnt exists", async ()=>{
-            let db = createDb();
-            let error = false;
-            try{
-                await db.getCollection("nothelloworld");
-            } catch{
-                error = true;
-            }
-            if(error===false) throw new Error("there was no error");
-        });
-
-        it("throw error if collection already exists", async ()=>{
-            let db = createDb();
-            let error = false;
-            try{
-                await db.createCollection("helloworld");
-                await db.createCollection("helloworld");
-            } catch{
-                error = true;
-            }
-            if(error===false) throw new Error("there was no error");
-        });
-
-        it("returning parsed collections", ()=>{
-            let db = createDb();
-            if(!db.collections) throw new Error("No collections");
-        });
+describe("FS", () => {
+  describe("collections", () => {
+    after(() => {
+      for (let folder of folderstodelete) {
+        removefiles(folder);
+      }
     });
+
+    it("create collections", async () => {
+      let db = createDb();
+      await db.createCollection("helloworld");
+      if (db.colNames.indexOf("helloworld") < 0)
+        throw new Error("collection is not created");
+    });
+
+    it("get collection by name", async () => {
+      let db = createDb();
+      await db.createCollection("helloworld");
+      if (!(await db.getCollection("helloworld")))
+        throw new Error("collection is not created");
+    });
+
+    it("throw error if collection doesnt exists", async () => {
+      let db = createDb();
+      let error = false;
+      try {
+        await db.getCollection("nothelloworld");
+      } catch {
+        error = true;
+      }
+      if (error === false) throw new Error("there was no error");
+    });
+
+    it("throw error if collection already exists", async () => {
+      let db = createDb();
+      let error = false;
+      try {
+        await db.createCollection("helloworld");
+        await db.createCollection("helloworld");
+      } catch {
+        error = true;
+      }
+      if (error === false) throw new Error("there was no error");
+    });
+
+    it("returning parsed collections", () => {
+      let db = createDb();
+      if (!db.collections) throw new Error("No collections");
+    });
+  });
 });
