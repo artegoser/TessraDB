e332b6e029287be77561521077b88ad62dfba0d3
Artemy <artegoser@gmail.com>
2022-01-05 16:01:31 +0300

    feat(collection): insert document function (db.insert())

--- src/collection.ts
+++ src/collection.ts
@@ -1,17 +1,50 @@
 import * as JSONS from "JSONStream";
 import * as fs from "fs";
+import * as aw from "./atomWrite";
 
 /**
  * Class of Tessra Collection
- * Performs the role of a collection object to perform 
- * all the functions of TessraDB collections
- * (find, delete, insert, update)
+ * Performs all the functions of TessraDB 
+ * collections (find, delete, insert, update, etc.)
  */
 export class TessraCollection {
-    public name:string;
-    public path:string;
-    constructor(name:string, path:string){
-        this.name = name;
-        this.path = path;
+  public name: string;
+  public path: string;
+  #locked : boolean = false;
+  constructor(name: string, path: string) {
+    this.name = name;
+    this.path = path;
+    this.#init();
+  }
+  /**
+   * Inititalize the collection
+   */
+  #init(): void {
+    if (!fs.existsSync(this.path)) {
+      aw.writeFileSync(this.path, "[]");
     }
+  }
+  /**
+   * Insert a document to collection
+   * @param doc Document to insert
+   */
+  public insert(doc: Object): Promise<void> {
+      return new Promise(async (res, rej)=>{
+        this.#locked = true;
+        let readStream = fs.createReadStream(this.path);
+        let objReadStream = JSONS.parse([true]);
+        let objWriteStream = await aw.objWriteStream(this.path);
+        readStream.pipe(objReadStream);
+
+        objReadStream.on("data", (data)=>{
+            objWriteStream.stream.write(data);
+        });
+
+        objReadStream.on("end", async ()=>{
+            objWriteStream.stream.write(doc);
+            await objWriteStream.end();
+            res();
+        });
+      });
+  }
 }
