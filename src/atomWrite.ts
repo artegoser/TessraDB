864383119772f6cb74d0df1d005232b801d77a7c
Artemy <artegoser@gmail.com>
2022-01-06 19:32:19 +0300

    fix: objWriteStream is not destroy fileStream after end

--- src/atomWrite.ts
+++ src/atomWrite.ts
@@ -1,34 +1,37 @@
-import * as fs from 'fs'
-import * as path from 'path'
-import { atomWriteStream } from './interfaces';
+import * as fs from "fs";
+import * as path from "path";
+import { atomWriteStream } from "./interfaces";
 import * as JSONS from "JSONStream";
 
-function getTemp(name:string): string {
-  return path.join(path.dirname(name), path.basename(name) + '.tetmp')
+function getTemp(name: string): string {
+  return path.join(path.dirname(name), path.basename(name) + ".tetmp");
 }
 
-export async function writeFile(fileName:string, data:string): Promise<void> {
+export async function writeFile(fileName: string, data: string): Promise<void> {
   let tempFileName = getTemp(fileName);
-  await fs.promises.writeFile(tempFileName, data, 'utf-8');
+  await fs.promises.writeFile(tempFileName, data, "utf-8");
   await fs.promises.rename(tempFileName, fileName);
 }
 
-export function writeFileSync(fileName:string, data:string): void {
+export function writeFileSync(fileName: string, data: string): void {
   let tempFileName = getTemp(fileName);
-  fs.writeFileSync(tempFileName, data, 'utf-8');
+  fs.writeFileSync(tempFileName, data, "utf-8");
   fs.renameSync(tempFileName, fileName);
 }
 
-export async function objWriteStream(fileName:string): Promise<atomWriteStream> {
+export async function objWriteStream(
+  fileName: string
+): Promise<atomWriteStream> {
   let tempFileName = getTemp(fileName);
   let writeStream = fs.createWriteStream(tempFileName);
-  let objStream = JSONS.stringify("[",",","]");
+  let objStream = JSONS.stringify("[", ",", "]");
   objStream.pipe(writeStream);
 
-  async function end(){
+  async function end() {
     objStream.end();
     await fs.promises.rename(tempFileName, fileName);
+    writeStream.destroy();
   }
 
-  return {stream: objStream, end};
+  return { stream: objStream, end };
 }
